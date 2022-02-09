165beb60f819a1b31da20a18b55330e3b5e6e6c5
Jonas L. Holm <duch11@users.noreply.github.com>
2022-02-09 21:21:11 +0100

    Update download_images.py

--- download_images.py
+++ download_images.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 url_with_page = "https://www.uahirise.org/catalog/captions/index.php?page="
 
-resolution = "1440"
+resolution = "4500"
 path = "images/"
 image_url = "https://static.uahirise.org/images/wallpaper/"+resolution+"/"
 
@@ -15,16 +15,38 @@ def download_image(image_url, file_name):
     image_data = requests.get(image_url)
     open(path+file_name, 'wb').write(image_data.content)
 
-#imgs = [{"url":"URL","filename":"FILE"}]
+def download_page(page_url):
+    page_data = requests.get(page_url)
+    return page_data
+
+
+
+
+page_urls = []
+pages = []
 imgs = []
+#imgs = [{"url":"URL","filename":"FILE"}]
 #print(imgs[0]["url"])
 #print(imgs[0]["filename"])
 
 for i in range(108):
-    print(i+1)
-    res = requests.get(url_with_page+str(i+1))
+    url_with_page_and_no = url_with_page+str(i+1)
+    print(url_with_page_and_no)
+    page_urls.append(url_with_page_and_no)
 
-    html = BeautifulSoup(res.content, "html.parser")
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    threads = []
+    for page_url in page_urls:
+        print(page_url)
+        threads.append(executor.submit(download_page, page_url))
+    for thread in concurrent.futures.as_completed(threads):
+        res=thread.result()
+        pages.append(res)
+        print(res.text[ : 8])
+
+for page in pages:
+    html = BeautifulSoup(page.content, "html.parser")
     image_cells = html.find_all("td", class_="catalog-cell-images")
 
     for cell in image_cells:
